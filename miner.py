78e76bc92bf03635125a7dbce5c3559b3c6562cc
Rami bond <ramibaydoun@proton.me>
2025-03-09 00:21:17 +0000

    Update miner.py

--- miner.py
+++ miner.py
@@ -3,7 +3,7 @@ import json
 import time
 
 # Configuración
-SERVER_URL = "http://127.0.0.1:5000"    
+SERVER_URL = "http://83.59.160.219:5000"    
 MINER_ADDRESS = "tu_direccion_de_miner"  # Dirección donde recibirás recompensas
 
 # Función para obtener transacciones pendientes
